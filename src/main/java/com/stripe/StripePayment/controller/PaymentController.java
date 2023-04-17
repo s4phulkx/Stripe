5dfd4ed15bd7d97362b0b687a1f1ccd84007090a
lnxxxxxxxx <leandro.luna25@hotmail.com>
2023-04-17 09:37:44 -0300

    add controller component

---
+++ src/main/java/com/stripe/StripePayment/controller/PaymentController.java
@@ -0,0 +1,50 @@
+package com.stripe.StripePayment.controller;
+
+
+
+import com.stripe.StripePayment.dto.PaymentDto;
+import com.stripe.StripePayment.service.IPaymentService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/payment")
+@CrossOrigin(origins = "*")
+public class PaymentController {
+
+    @Autowired
+    IPaymentService paymentService;
+
+    @PostMapping("/paymentintent")
+    public ResponseEntity<String> payment(@RequestBody PaymentDto paymentIntentDto) throws StripeException {
+        PaymentIntent paymentIntent = paymentService.paymentIntent(paymentIntentDto);
+        String paymentStr = paymentIntent.toJson();
+        return new ResponseEntity<String>(paymentStr, HttpStatus.OK);
+    }
+
+    @PostMapping("/confirm/{id}")
+    public ResponseEntity<String> confirm(@PathVariable("id") String id) throws StripeException {
+        PaymentIntent paymentIntent = paymentService.confirm(id);
+        String paymentStr = paymentIntent.toJson();
+        return new ResponseEntity<String>(paymentStr, HttpStatus.OK);
+    }
+
+    @PostMapping("/cancel/{id}")
+    public ResponseEntity<String> cancel(@PathVariable("id") String id) throws StripeException {
+        PaymentIntent paymentIntent = paymentService.cancel(id);
+        String paymentStr = paymentIntent.toJson();
+        return new ResponseEntity<String>(paymentStr, HttpStatus.OK);
+    }
+}
+
+
+
+
